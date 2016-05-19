0d46fade10f18a1ff9f63db18c6307c7da57e318
Roahhh <ronghualui@gmail.com>
2016-05-19 15:24:54 +0800

    Updated Home Page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | RolePlay"
+    assert_select "title", "RolePlay"
   end
 
   test "should get about" do
