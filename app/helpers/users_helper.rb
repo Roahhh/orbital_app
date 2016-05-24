0f6bf88a2bcb6507814bff63c9fde34dd14ca462
skynobleu <ngweijie94@gmail.com>
2016-05-24 13:56:21 +0800

    Modified users_helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  
+  exp_mul = 1.15
   # Returns the Gravatar for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
@@ -9,5 +9,14 @@ module UsersHelper
 
   def full_name(user)
   	 return user.first_name + " " + user.last_name
+  end
+
+  def total_exp_level(lvl)
+  	return 100 #(100(1 - exp_mul ** (lvl - 1)) / (1 - exp_mul))
+  end
+
+  def exp_remain(user)
+  	next_lvl = 100 * (1.2 ** (user.lvl - 1))
+  	return (user.exp - total_exp_level) / next_lvl 
   end
 end
