d69c831e1bc4027a622ff10184db22ead353114e
Roahhh <ronghualui@gmail.com>
2016-05-29 22:37:12 +0800

    Editted + symbol view rights

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :show]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :show, :add_str, :add_agi, :add_vit, :add_int]
   before_action :correct_user,   only: [:edit, :update, :add_str, :add_agi, :add_vit, :add_int]
   before_action :admin_user,     only: [:destroy, :create]
 
@@ -115,6 +115,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user) || current_user.admin?
+    end
+
+    def only_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     def admin_user
